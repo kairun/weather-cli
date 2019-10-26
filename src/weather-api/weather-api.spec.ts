5777aa53eb1fcda65930babfc432eb4ead023e4d
Seongmin Jeong <kairunkr@gmail.com>
2019-10-26 22:22:49 +1100

    Refactored some code of spec for WeatherApi

--- src/weather-api/weather-api.spec.ts
+++ src/weather-api/weather-api.spec.ts
@@ -1,24 +1,23 @@
 import 'jest';
-import { SinonStub } from 'sinon';
 import { WeatherApi } from './weather-api';
 import { UnexpectedInputError } from './errors';
 
 describe('WeatherApi', () => {
   let weatherApi: WeatherApi;
-  let getStub: SinonStub;
 
   beforeAll(() => {
     let WeatherApi = mockJest();
 
     weatherApi = new WeatherApi('4c18248c48f4a6f471cad850bd29285c');
-    getStub = require('axios').default.get;
   });
 
   describe('getWeatherToday', () => {
     it('should call axios.get', async () => {
-      await weatherApi.getWeatherToday('strathfield');
+      let getStub = require('axios').default.get;
 
+      await weatherApi.getWeatherToday('strathfield');
       expect(getStub.called).toBeTruthy();
+
       getStub.resetHistory();
     });
 
